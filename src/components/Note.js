457cb68c7f769b0eae5cd3217cad4bbb117be170
flpcury <26260549+flpcury@users.noreply.github.com>
2021-08-31 17:44:44 +0900

    feat: Port features from webapp
    Co-authored-by: Michelle <mdeniselau@gmail.com>
    Co-authored-by: rituraj8503 <rituraj8@uw.edu>

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import host from "../config"
+import host from "../config";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { FaTrash, FaPencilAlt } from "react-icons/fa";
+import moment from "moment";
+import { LinkPreview } from "@dhaiwat10/react-link-preview";
+import ReactMarkdown from "react-markdown";
+import ShareModal from "./ShareModal";
 
-
-export default function Note({ note, token, setShowEditForm, setSelectedNote}) {
-   const handleShow = () => {
-     console.log("clicked edit button")
-     setSelectedNote(note)
-     setShowEditForm(true)
-  //   dispatch({ type: "SET_SELECTED_NOTE", payload: note });
-  //   dispatch({ type: "TOGGLE_SHOW_EDIT_FORM" });
-   };
+export default function Note({
+  note,
+  token,
+  setShowEditForm,
+  setSelectedNote,
+  setShowShareSuccess,
+}) {
+  const [showUserName, setShowUserName] = useState(false);
+  const handleShow = () => {
+    console.log("clicked edit button");
+    setSelectedNote(note);
+    setShowEditForm(true);
+  };
 
   const handleDelete = async (e) => {
     const id = note.id;
@@ -24,13 +32,37 @@ export default function Note({ note, token, setShowEditForm, setSelectedNote}) {
   };
 
   return (
-    <Card style={{ width: '90vw', margin: "0 auto" }}>
+    <Card style={{ width: "90vw", margin: "0 auto" }}>
       <Card.Body>
         <Card.Title>{note.title}</Card.Title>
-        <Card.Text>
-          {note.content}
-        </Card.Text>
-        <Card.Link href={note.url}>{note.url}</Card.Link>
+        {typeof note.image != "undefined" && (
+          <div
+            className="circle-pic profile-pic-small"
+            style={{
+              backgroundImage: `url(${note.image || "../profile-default.png"})`,
+            }}
+            onMouseEnter={() => setShowUserName(true)}
+            onMouseLeave={() => setShowUserName(false)}
+          ></div>
+        )}
+        {showUserName && (
+          <p
+            style={{
+              position: "absolute",
+              backgroundColor: "pink",
+            }}
+          >
+            {note.name}
+          </p>
+        )}
+        <ReactMarkdown>{note.content}</ReactMarkdown>
+        <Card.Subtitle className="mb-2 text-muted">
+          {"Created: " + moment(note.created_at).fromNow()}
+        </Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted">
+          {"Modified: " + moment(note.modified_at).fromNow()}
+        </Card.Subtitle>
+        <LinkPreview url={note.url} width="55vw" />
         <div className="note-buttons">
           <Button variant="danger" onClick={handleDelete}>
             <FaTrash />
@@ -38,6 +70,11 @@ export default function Note({ note, token, setShowEditForm, setSelectedNote}) {
           <Button variant="primary" onClick={handleShow}>
             <FaPencilAlt />
           </Button>
+          <ShareModal
+            note={note}
+            setShowShareSuccess={setShowShareSuccess}
+            token={token}
+          />
         </div>
       </Card.Body>
     </Card>
